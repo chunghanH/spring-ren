294c25fe9c72c6d531fe3561649c80bbfad035b7
yous421 <yous421@gmail.com>
2022-07-02 13:45:46 +0800

    spring initialzr

---
+++ src/main/java/com/han/springren/SpringrenApplication.java
@@ -0,0 +1,13 @@
+package com.han.springren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrenApplication.class, args);
+	}
+
+}
